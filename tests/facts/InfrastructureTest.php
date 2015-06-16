179a30457accb1916153b53516e9ab3da8e1e8ee
Van Mittal-Henkle <van@kiva.org>
2015-06-16 13:15:42 -0700

    VER-9 add a basic phpunit test and README.

---
+++ tests/facts/InfrastructureTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: van
+ * Date: 6/16/15
+ * Time: 12:15 PM
+ */
+require 'vendor/autoload.php';
+
+class InfrastructureTest extends PHPUnit_Framework_TestCase {
+
+	public function testSomething() {
+		$this->assertEquals("hello world","hello world");
+	}
+}
+
+
