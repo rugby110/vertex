d54d7ab899517b57e6e951cdf7730bfcce572108
Van Mittal-Henkle <van@kiva.org>
2015-06-18 14:55:43 -0700

    VER-4 update dev sandboxes sql.

--- admin/developer_sandbox_creation.sql
+++ admin/developer_sandbox_creation.sql
@@ -4,5 +4,6 @@ grant all on schema van_sandbox to van with grant option;
 create schema IF NOT EXISTS nikkiw_sandbox AUTHORIZATION nikkiw;
 grant all on schema nikkiw_sandbox to nikkiw with grant option;
 
+-- verse_qa is a temporary standin for ods_kiva which is where raw kiva db tables live
 grant usage on schema verse_qa to van, nikkiw;
 grant select on all tables in schema verse_qa to van, nikkiw;
