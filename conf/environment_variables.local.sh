09676782a8e8748134ae35f9051267c78dd5b192
Van Mittal-Henkle <van@kiva.org>
2015-06-19 12:46:48 -0700

    cleanup

--- conf/environment_variables.local.sh
+++
@@ -1,7 +0,0 @@
-# this is local only and doesn't go into git
-vertex_vertica_database=verse
-vertex_vertica_host=localhost
-vertex_vertica_port=3322
-vertex_vertica_user=van
-vertex_vertica_password=kivar0cks
-vertex_vertica_target_schema=van_sandbox
