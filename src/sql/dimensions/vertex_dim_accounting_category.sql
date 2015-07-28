1d968ecb769fda75ac46b910fafab3363f8f5496
Van Mittal-Henkle <van@kiva.org>
2015-07-28 15:59:49 -0700

    VER-16 added creation and population of vertex_dim_accounting_category.

---
+++ src/sql/dimensions/vertex_dim_accounting_category.sql
@@ -0,0 +1,12 @@
+drop table if exists vertex_dim_accounting_category;
+
+create table vertex_dim_accounting_category (
+        id                      integer not null,
+        accounting_category     varchar(40)
+);
+
+insert into vertex_dim_accounting_category values(0, 'none');
+insert into vertex_dim_accounting_category values(1, 'self_directed');
+insert into vertex_dim_accounting_category values(2, 'managed_account');
+insert into vertex_dim_accounting_category values(3, 'kmf');
+insert into vertex_dim_accounting_category values(4, 'daf');
