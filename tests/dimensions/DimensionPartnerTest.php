0b4801e2c82044e3aa4764d934fd720872b52f8e
Nikki Woelk <nikkiw@kiva.org>
2015-08-19 10:30:33 -0700

    VER-72 corrected case-sensitive condition and made test deterministic

--- tests/dimensions/DimensionPartnerTest.php
+++ tests/dimensions/DimensionPartnerTest.php
@@ -19,49 +19,20 @@ class DimensionPartnerTest extends Kiva\Vertex\Testing\VertexTestCase {
 		$this->assertEquals($count_from_kiva_ods,$count_from_vertex);
 	}
 
-	/*public function testRandomSample() {
-
-		$random_ids = $this->_setupHelper();
-		echo $random_ids;
-
+	public function testSample() {
 		$result = $this->db->query("select partner_id, name, status, first_fundraising_day_id, charges_fees_interest, enable_relisting
 			from $this->vertex_schema.vertex_dim_partner
-			where partner_id in ($random_ids)
+			where partner_id in (322,243,176,182,96,270,220,168,281,451)
 			order by partner_id");
 		$from_vertex = $result->fetchAll();
 
 
-		$result = $this->db->query("select partner_id, name, status, first_fundraising_day_id, charges_fees_interest, enable_relisting
+		$result = $this->db->query("select distinct partner_id, name, status, first_fundraising_day_id, charges_fees_interest, enable_relisting
 			from $this->reference_schema.verse_dim_partner
-			where partner_id in ($random_ids)
+			where partner_id in (322,243,176,182,96,270,220,168,281,451)
 			order by partner_id");
 		$from_dim = $result->fetchAll();
 
 		$this->assertSame($from_dim,$from_vertex);
-
-	}*/
-
-	private function _setupHelper() {
-
-		$random_result = $this->db->query("SELECT id
-			FROM
-     		(SELECT id
-            , RANDOM() AS RandomNumber
-     		FROM $this->reference_schema.verse_ods_kiva_partner
-     		WHERE RANDOMINT((SELECT COUNT(*) FROM $this->reference_schema.verse_ods_kiva_partner ))%100 = 42
-     		) AS T1
-			ORDER BY RandomNumber
-			LIMIT 5");
-		$random_ids = $random_result->fetchAll();
-
-		for ($i = 0, $len = count($random_ids); $i < $len; $i++) {
-			$id_array[] = $random_ids[$i]['id'];
-		}
-
-		$id_list = implode(",",$id_array);
-
-		return $id_list;
-
 	}
-
 }
