484dd686a1b55624a5bce7c7d5d8923c2ca2398c
Nikki Woelk <nikkiw@kiva.org>
2015-08-20 11:56:56 -0700

    VER-272 made test deterministic

--- tests/facts/FactManagedAccountTest.php
+++ tests/facts/FactManagedAccountTest.php
@@ -9,48 +9,35 @@ class FactManagedAccountTest extends Kiva\Vertex\Testing\VertexTestCase {
 		parent::tearDown();
 	}
 
+	public function testPromoAmountRedeemedCount() {
+		$result = $this->db->query("select count(1) as how_many
+			from $this->vertex_schema.vertex_fact_managed_account
+			where promo_redeemed_amt > 0");
+		$count_from_vertex = $result->fetchColumn();
 
-	public function testRandomSample() {
+		$result = $this->db->query("select count(1) as how_many
+			from $this->reference_schema.verse_dim_managed_account
+			where promo_redeemed_amt > 0");
+		$count_from_dim = $result->fetchColumn();
 
-		$random_ids = $this->_setupHelper();
-		//echo $random_ids . "\n";
+		$this->assertSame($count_from_dim,$count_from_vertex);
+	}
 
+
+	public function testSample() {
 		$result = $this->db->query("select fund_account_id, promo_fund_id, promo_redeemed_amt, total_amount_disbursed, total_amount_repaid, total_amount_donated, total_amount_lost, total_amount_refunded, number_loans
 			from $this->vertex_schema.vertex_fact_managed_account
-			where fund_account_id in ($random_ids)
-			order by fund_account_id");
+			where fund_account_id in (1473428, 1186366, 1731534, 1473411, 1666360, 990719, 807517, 1001501, 2166208, 942885)
+			order by fund_account_id, promo_fund_id");
 		$from_vertex = $result->fetchAll();
 
 
 		$result = $this->db->query("select fund_account_id, promo_fund_id, promo_redeemed_amt, total_amount_disbursed, total_amount_repaid, total_amount_donated, total_amount_lost, total_amount_refunded, number_loans
 			from $this->reference_schema.verse_dim_managed_account
-			where fund_account_id in ($random_ids)
-			order by fund_account_id");
+			where fund_account_id in (1473428, 1186366, 1731534, 1473411, 1666360, 990719, 807517, 1001501, 2166208, 942885)
+			order by fund_account_id, promo_fund_id");
 		$from_dim = $result->fetchAll();
 
 		$this->assertSame($from_dim,$from_vertex);
-
-	}
-
-
-	private function _setupHelper() {
-		$random_result = $this->db->query("SELECT fund_account_id
-			FROM
-     		(SELECT fund_account_id
-            , RANDOM() AS RandomNumber
-     		FROM $this->vertex_schema.vertex_fact_managed_account
-     		WHERE RANDOMINT((SELECT COUNT(*) FROM $this->vertex_schema.vertex_fact_managed_account ))%100 = 42
-     		) AS T1
-			ORDER BY RandomNumber
-			LIMIT 10");
-		$random_ids = $random_result->fetchAll();
-
-		for ($i = 0, $len = count($random_ids); $i < $len; $i++) {
-			$id_array[] = $random_ids[$i]['fund_account_id'];
-		}
-
-		$id_list = implode(",",$id_array);
-
-		return $id_list;
 	}
 }
