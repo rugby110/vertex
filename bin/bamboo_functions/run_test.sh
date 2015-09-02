2f01253d4f307507712eaa47ee18eff0c448f2ea
Jim Cooley <jcooley@kiva.org>
2015-09-02 12:22:20 -0700

    VER-273: running ddl multiple times as a workaround for VER-337

--- bin/bamboo_functions/run_test.sh
+++ bin/bamboo_functions/run_test.sh
@@ -8,8 +8,11 @@ function run_test () {
   source conf/environment_variables.sh
   echo "running mpm_migrations"
   run_mpm_migrations
-  echo "running ddl"
-  run_ddl
+  # VER-337 - need to run ddl 4 times as a workaround
+  for count in $(seq 4); do
+    echo "running ddl"
+    run_ddl
+  done
   echo "running permissions"
   run_permissions
   echo "running phpunit <bamboo_job_name> # where bamboo_job_name is $job_name"
