a4ee93e3b848edd6688c302fe0f8d2b7d40d26d1
Van Mittal-Henkle <van@kiva.org>
2015-09-09 10:15:04 -0700

    VER-299 add schemas for publishing verse data to vertex.

--- admin/schema_creation.sql
+++ admin/schema_creation.sql
@@ -4,5 +4,10 @@ grant all on schema vertex to vertex with grant option;
 grant all on schema ods_kiva to vertex with grant option;
 
 create schema IF NOT EXISTS verse AUTHORIZATION verse;
+create schema IF NOT EXISTS verse_publish AUTHORIZATION verse;
+create schema IF NOT EXISTS verse_live AUTHORIZATION verse;
 grant all on schema verse to verse with grant option;
+grant all on schema verse_publish to verse with grant option;
+grant all on schema verse_live to verse with grant option;
 
+grant create on database verticanow to verse; --for renaming schemas during verse publishing
