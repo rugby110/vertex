4da6c8023e759ed0fce3b6f78584f04bfbf41459
Dave Thomas <dthomas@kiva.org>
2015-09-09 13:22:00 -0700

    adding comment to dim_geo

--- src/sql/dimensions/vertex_dim_geo.sql
+++ src/sql/dimensions/vertex_dim_geo.sql
@@ -1,7 +1,12 @@
-/**
+/*
+
 NOTE:  the verse had a row with id = 0 and name = 'None'.
 If needed, we can probably do hard-coded insert with a UNION
+
+NOTE:  there seems to have been some work around adding lat/long, but these cols were empty in the verse.  this can be updated if need be.
+
  */
+
 create or replace view vertex_dim_geo as
 select distinct
 	t.id as geo_id,
