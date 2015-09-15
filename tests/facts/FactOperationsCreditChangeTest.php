c00c4e2ac0b2c5b69dbb9b41c663088e42ad67d4
Zahid Zaman <zahid@kiva.org>
2015-09-14 18:01:46 -0700

    VER-100 One more test.

--- tests/facts/FactOperationsCreditChangeTest.php
+++ tests/facts/FactOperationsCreditChangeTest.php
@@ -20,6 +20,20 @@ class FactOperationsCreditChangeTest extends Kiva\Vertex\Testing\VertexTestCase 
 		$this->assertEquals($count_from_ods,$count_from_fact);
 	}
 
+	public function testAdminUserIdCount() {
+		$result = $this->db->query("select count(1) as how_many
+			from $this->vertex_schema.vertex_fact_operations_credit_change
+			where admin_user_id > 0");
+		$count_from_vertex = $result->fetchColumn();
+
+		$result = $this->db->query("select count(1) as how_many
+			from $this->reference_schema.verse_ods_kiva_operations_credit_change
+			where admin_user_id > 0");
+		$count_from_verse = $result->fetchColumn();
+
+		$this->assertSame($count_from_verse,$count_from_vertex);
+	}
+
 	public function testSample() {
 		$result = $this->db->query("select operations_credit_change_id, create_day_id, effective_day_id
 			from $this->vertex_schema.vertex_fact_operations_credit_change
