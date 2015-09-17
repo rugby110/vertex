fd2c0842bacc2686fb08872441364cc8c95533f0
Zahid Zaman <zahid@kiva.org>
2015-09-16 19:51:05 -0700

    VER-122 Some minor cleanup.

--- src/sql/facts/vertex_fact_zip_operations_credit_change.sql
+++ src/sql/facts/vertex_fact_zip_operations_credit_change.sql
@@ -14,6 +14,6 @@ select occ.id as zip_operations_credit_change_id,
 	dim_cct.credit_change_type_id,
   occ.fx_rate_id
 from verse.verse_ods_zip_operations_credit_change occ
-inner join vertex_dim_credit_change_type dim_cct on dim_cct.type_name = occ.type and dim_cct.source_table_name = 'zip.operations_credit_change'
+left join vertex_dim_credit_change_type dim_cct on dim_cct.type_name = occ.type and dim_cct.source_table_name = 'zip.operations_credit_change'
 ;
 
--- tests/facts/FactZipOperationsCreditChangeTest.php
+++ tests/facts/FactZipOperationsCreditChangeTest.php
@@ -13,7 +13,7 @@ class FactZipOperationsCreditChangeTest extends Kiva\Vertex\Testing\VertexTestCa
 		$result = $this->db->query("select count(1) as how_many from $this->vertex_schema.vertex_fact_zip_operations_credit_change");
 		$count_from_fact = $result->fetchColumn();
 
-		$result = $this->db->query("select count(1) as how_many from verse.verse_ods_zip_operations_credit_change");
+		$result = $this->db->query("select count(1) as how_many from verse.verse_fact_zip_operations_credit_change");
 		$count_from_ods = $result->fetchColumn();
 
 		//echo "count from fact: " . $count_from_fact . " count from ods: $count_from_ods\n";
