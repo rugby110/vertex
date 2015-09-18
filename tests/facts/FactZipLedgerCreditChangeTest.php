dd021a855fd52cf7deaaaba7ad6e3e26ec222dcf
Zahid Zaman <zahid@kiva.org>
2015-09-18 11:31:46 -0700

    VER-121 Updating reference to reference schema.

--- tests/facts/FactZipLedgerCreditChangeTest.php
+++ tests/facts/FactZipLedgerCreditChangeTest.php
@@ -13,7 +13,7 @@ class FactZipLedgerCreditChangeTest extends Kiva\Vertex\Testing\VertexTestCase {
 		$result = $this->db->query("select count(1) as how_many from $this->vertex_schema.vertex_fact_zip_ledger_credit_change");
 		$count_from_fact = $result->fetchColumn();
 
-		$result = $this->db->query("select count(1) as how_many from verse.verse_fact_zip_ledger_credit_change");
+		$result = $this->db->query("select count(1) as how_many from $this->reference_schema.verse_fact_zip_ledger_credit_change");
 		$count_from_ods = $result->fetchColumn();
 
 		//echo "count from fact: " . $count_from_fact . " count from ods: $count_from_ods\n";
