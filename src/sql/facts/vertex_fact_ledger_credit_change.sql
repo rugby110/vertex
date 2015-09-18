d5d57baf9bf56d9c2b1bec3b6bb8f5a3a5fff890
Nikki Woelk <nikkiw@kiva.org>
2015-09-18 12:21:44 -0700

    VER-92 changed join from inner to left

--- src/sql/facts/vertex_fact_ledger_credit_change.sql
+++ src/sql/facts/vertex_fact_ledger_credit_change.sql
@@ -15,6 +15,6 @@ select id as ledger_credit_change_id,
 	fa.accounting_category_id
 				
 from ledger_credit_change lcc
-inner join vertex_dim_fund_account_accounts fa 
+left join vertex_dim_fund_account_accounts fa 
         on fa.fund_account_id=COALESCE(lcc.fund_account_id, 0) --fund_account_id can be null in ods, so force it to 0
 ;
