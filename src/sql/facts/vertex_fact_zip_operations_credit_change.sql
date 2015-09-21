80eba69543a6cbb06ee4619b68304a16de848413
Zahid Zaman <zahid@kiva.org>
2015-09-21 15:49:30 -0700

    VER-122 More robust tests and the bugs they revealed.

--- src/sql/facts/vertex_fact_zip_operations_credit_change.sql
+++ src/sql/facts/vertex_fact_zip_operations_credit_change.sql
@@ -1,18 +1,18 @@
 create or replace view vertex_fact_zip_operations_credit_change as
 
 select occ.id as zip_operations_credit_change_id,
-  occ.effective_time,
+	occ.effective_time,
 	TO_CHAR(TO_TIMESTAMP(occ.effective_time), 'YYYYMMDD')::INT as effective_day_id,
 	occ.create_time,
 	TO_CHAR(TO_TIMESTAMP(occ.create_time), 'YYYYMMDD')::INT as create_day_id,
-	occ.price,
+	cast(occ.price as numeric(36,2)) as price,
 	occ.currency,
-	occ.description,
-	occ.pp_txn_id,
-	occ.mpesa_receipt_no,
-	occ.admin_user_id,
+	COALESCE(occ.description,'') as description,
+	COALESCE(occ.pp_txn_id,'') as pp_txn_id,
+	COALESCE(occ.mpesa_receipt_no,'') as mpesa_receipt_no,
+	COALESCE(occ.admin_user_id, 0) as admin_user_id,
 	dim_cct.credit_change_type_id,
-  occ.fx_rate_id
+	occ.fx_rate_id
 from verse.verse_ods_zip_operations_credit_change occ
 left join vertex_dim_credit_change_type dim_cct on dim_cct.type_name = occ.type and dim_cct.source_table_name = 'zip.operations_credit_change'
 ;
